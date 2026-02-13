1d10fbb7814712d20d07f9712fd7a8e8302e95a7
Alfred01-HUB <alfredolabanji066@gmail.com>
2026-02-13 23:48:11 +0100

    Added a js interactive animation to page

--- Desktop/mini-porfolio/Javascript/script.js
+++ Desktop/mini-porfolio/Javascript/script.js
@@ -1,16 +1,30 @@
+const text = "I’m Oracl3, a Full-Stack Web Developer based in Nigeria";
+const speed = 80;
+const displayTimes = 3;
 
-  const text = "I’m Oracl3, a Full-Stack Web Developer based in Nigeria";
-  const speed = 80; // typing speed in ms
+const typingElement = document.getElementById("typing-text");
 
-  let index = 0;
-  const typingElement = document.getElementById("typing-text");
+let index = 0;
+let currentCount = 0;
 
-  function typeText() {
-    if (index < text.length) {
-      typingElement.textContent += text.charAt(index);
-      index++;
-      setTimeout(typeText, speed);
-    } 
+function typeText() {
+  if (index < text.length) {
+    typingElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeText, speed);
+  } else {
+    // typing finished once
+    currentCount++;
+
+    if (currentCount < displayTimes) {
+      // reset and type again
+      setTimeout(() => {
+        typingElement.textContent = "";
+        index = 0;
+        typeText();
+      },10000); // pause before restarting
+    }
   }
-  typeText();
+}
 
+typeText();
